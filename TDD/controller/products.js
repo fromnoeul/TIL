85ac92217837bf3c1ad68ba1aa78d250ad7b1085
fromnoeul <1999jong@naver.com>
2021-09-09 20:24:33 +0900

    [TIL_210909]CREATE unit test 'should return json body in response && IMPLEMENT correspond code'

--- TDD/controller/products.js
+++ TDD/controller/products.js
@@ -1,6 +1,6 @@
 const ProductModel = require('../models/Product');
 
 exports.createProduct = (req, res, next) => {
-  ProductModel.create(req.body);
-  res.status(201).send();
+  const createdProduct = ProductModel.create(req.body);
+  res.status(201).json(createdProduct);
 };
