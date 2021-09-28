46ccdada0e104cccbf517f7ead8671fb3f99f748
fromnoeul <1999jong@naver.com>
2021-09-28 18:40:21 +0900

    ADD error handling case for integrated test

--- TDD/server.js
+++ TDD/server.js
@@ -18,6 +18,10 @@ mongoose
 
 app.use(express.json());
 app.use('/api/products', productsRoutes);
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
+});
+
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
